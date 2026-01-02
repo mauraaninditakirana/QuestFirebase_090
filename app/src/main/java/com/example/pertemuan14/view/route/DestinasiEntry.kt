2895d61bf93c9912c81b7f974dc0bb68fc344f0a
mauraaninditakirana <kiranamaura351@gmail.com>
2026-01-02 16:43:18 +0700

    Membuat route untuk DestinasiEntry.

---
+++ app/src/main/java/com/example/pertemuan14/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.pertemuan14.view.route
+
+import com.example.pertemuan14.view.route.DestinasiNavigasi
+import com.example.pertemuan14.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
