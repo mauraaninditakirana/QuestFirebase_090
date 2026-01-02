add18a17fd3295e591a7269dec76a89359841bc0
mauraaninditakirana <kiranamaura351@gmail.com>
2026-01-02 16:43:06 +0700

    Membuat route untuk DestinasiHome.

---
+++ app/src/main/java/com/example/pertemuan14/view/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.pertemuan14.view.route
+
+import com.example.pertemuan14.R
+import com.example.pertemuan14.view.route.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
